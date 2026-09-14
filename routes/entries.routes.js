768ce21b5a866d58dcff7d5bc32587108ba86edc
A-Alsaffaf <ahmedy.saffaf@gmail.com>
2026-09-14 16:28:07 +0300

    feat: add create entry page and link the routes

--- routes/entries.routes.js
+++ routes/entries.routes.js
@@ -1,5 +1,23 @@
+// imports
 const router = require('express').Router()
+const Entry = require('../models/Entry')
+const isSignedIn = require("../middleware/is-signed-in");
 
+// routes
+router.get('/new',isSignedIn, (req,res) => {
+    res.render('./entries/create-entry.ejs')
+})
+
+router.post('/', isSignedIn, async (req, res) => {
+    req.body.isPublic = Boolean(req.body.isPublic)
+    const createdEntry = await Entry.create({
+        title: req.body.title,
+        entryBody: req.body.entryBody,
+        isPublic: req.body.isPublic,
+        owner: req.session.user._id
+    })
+    res.redirect('/')
+})
 
 
 module.exports = router
