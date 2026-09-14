f174d9923019ebea28331d5ac552282504b23df7
A-Alsaffaf <ahmedy.saffaf@gmail.com>
2026-09-14 13:34:31 +0300

    feat(db): add Entry schema and model, create routes file for the model

---
+++ models/Entry.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const entrySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: 100
+    },
+    entryBody: {
+        type: String,
+        required: true,
+        maxLength: 350,
+    },
+    isPublic: {
+        type: Boolean,
+        default: false
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {timestamps: true})
+
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+module.exports = Entry
